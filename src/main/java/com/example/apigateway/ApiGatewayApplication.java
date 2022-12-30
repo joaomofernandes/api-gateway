b6f1b5e1bd48033c7f46a23b00a399be0e57654d
joaomiol <joao-miguel.oliveira-fernandes@capgemini.com>
2022-12-30 17:07:20 +0000

    Final Changes

--- src/main/java/com/example/apigateway/ApiGatewayApplication.java
+++ src/main/java/com/example/apigateway/ApiGatewayApplication.java
@@ -2,8 +2,10 @@ package com.example.apigateway;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.ComponentScan;
 
 @SpringBootApplication
+@ComponentScan({"com.example.apigateway.configuration"})
 public class ApiGatewayApplication {
 
 	public static void main(String[] args) {
